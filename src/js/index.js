e35cf64682bca51f9d1a3af1f56a29c2ce606a07
mke2111 <roymke134@gmail.com>
2021-02-13 17:34:42 +0300

    add all city info properties

--- src/js/index.js
+++ src/js/index.js
@@ -20,7 +20,7 @@ root.appendChild(userInput());
 // Get weather data
 
 async function getData() {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kampala&appid=49257f6591cfc3ed8daf0b5970d519cb`, 
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=49257f6591cfc3ed8daf0b5970d519cb`, 
     { mode: "cors" }
   );
 
@@ -53,49 +53,80 @@ const weatherCard = data => {
   card.classList.add('pt-6', 'flex', 'flex-row', 'content-center');
 
   const cardContainer = document.createElement('div');
-  cardContainer.classList.add('border', 'border-red-400', 'rounded-lg', 'p-4', 'w-3/6', 'mx-auto', 'px-auto', 'text-center');
+  cardContainer.classList.add('border', 'border-red-400', 'rounded-lg', 'p-4', 'w-5/6', 'mx-auto', 'px-auto', 'text-center');
 
+  // City time
   const cardTime = document.createElement('h2');
-  cardTime.classList.add('text-3xl');
+  cardTime.classList.add('text-2xl', 'text-white');
   cardTime.innerHTML = getLocalTime(data);
 
+  // City Name
   const cardCity = document.createElement('h2');
   cardCity.classList.add('text-4xl', 'text-blue-900', 'card-city');
   cardCity.innerText = `${data.name}, ${data.sys.country}`;
   
+  // City properties container
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('flex', 'justify-around', 'pt-6');
   
-  const cardTempDiv = document.createElement('div');
+  const cardTempDiv = document.createElement('div'); // Temperature
 
   const cardTemp = document.createElement('h2');
-  cardTemp.classList.add();
+  cardTemp.classList.add('text-2xl', 'text-white');
   cardTemp.innerHTML = `<h2>Temperature</h2>`;
 
   const temp = document.createElement('h2');
-  temp.classList.add();
+  temp.classList.add('text-3xl');
   temp.innerHTML = `<h2>${temperature.toString()}&#176;C</h2>`;
 
-  const cardIconDiv = document.createElement('div');
+  const cardHumDiv = document.createElement('div'); // Humidity
+
+  const cardHum = document.createElement('h2');
+  cardHum.classList.add('text-2xl', 'text-white');
+  cardHum.innerHTML = `<h2>Humidity</h2>`;
+
+  const humi = document.createElement('h2');
+  humi.classList.add('text-3xl');
+  humi.innerHTML = `<h2>${data.main.humidity}</h2>`;
+
+  const cardPreDiv = document.createElement('div'); // Pressure
+
+  const cardPressure = document.createElement('h2');
+  cardPressure.classList.add('text-2xl', 'text-white');
+  cardPressure.innerHTML = `<h2>Pressure</h2>`;
+
+  const pressure = document.createElement('h2');
+  pressure.classList.add('text-3xl');
+  pressure.innerHTML = `<h2>${data.main.pressure}</h2>`;
+
+  const cardIconDiv = document.createElement('div'); // Wind speed
 
   const cardIcon = document.createElement('h2');
-  cardIcon.classList.add();
+  cardIcon.classList.add('text-2xl', 'text-white');
   cardIcon.innerHTML = `Wind Speed`;
 
   const speed = document.createElement('h2');
-  speed.classList.add();
+  speed.classList.add('text-3xl');
   speed.innerHTML = `${data.wind.speed} M/H`;
 
-  const cardCloudDiv = document.createElement('div');
+  const cardCloudDiv = document.createElement('div'); // Description
 
   const cardCloud = document.createElement('h2');
-  cardCloud.classList.add();
-  cardCloud.innerText = `Clouds description`;
+  cardCloud.classList.add('text-2xl', 'text-white');
+  cardCloud.innerText = `Cloud description`;
 
   const cloud = document.createElement('h2');
-  cloud.classList.add();
+  cloud.classList.add('text-3xl');
   cloud.innerText = `${data.weather[0]['description']}`;
 
   cardTempDiv.appendChild(cardTemp);
   cardTempDiv.appendChild(temp);
+
+  cardHumDiv.appendChild(cardHum);
+  cardHumDiv.appendChild(humi);
+
+  cardPreDiv.appendChild(cardPressure);
+  cardPreDiv.appendChild(pressure);
 
   cardIconDiv.appendChild(cardIcon);
   cardIconDiv.appendChild(speed);
@@ -105,9 +136,12 @@ const weatherCard = data => {
 
   cardContainer.appendChild(cardTime);
   cardContainer.appendChild(cardCity);
-  cardContainer.appendChild(cardTempDiv);
-  cardContainer.appendChild(cardIconDiv);
-  cardContainer.appendChild(cardCloudDiv);
+  cardInfo.appendChild(cardTempDiv);
+  cardInfo.appendChild(cardHumDiv);
+  cardInfo.appendChild(cardPreDiv);
+  cardInfo.appendChild(cardIconDiv);
+  cardInfo.appendChild(cardCloudDiv);
+  cardContainer.appendChild(cardInfo);
   card.appendChild(cardContainer);
   root.appendChild(card);
 
